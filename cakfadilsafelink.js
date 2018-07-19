09b4c9a27fd387ed4a7f3035d403a5886b1ae28d
cakfadilstreamov21 <40716142+cakfadilstreamov21@users.noreply.github.com>
2018-07-19 17:47:27 +0700

    Update cakfadilsafelink.js

--- cakfadilsafelink.js
+++ cakfadilsafelink.js
@@ -168,7 +168,7 @@ function startTimer() {
   setTimeout(startTimer, 1000);
 }
 function checkSecond(sec) {
-  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 10 && sec >= 0) {sec = "5" + sec}; // add zero in front of numbers < 10
   if (sec < 0) {sec = "59"};
   return sec;
 }
